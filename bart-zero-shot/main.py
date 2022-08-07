888b07f0279fb9f5827f7b9ef863192d8b2482cc
John <johnbt@runbox.com>
2022-08-07 17:46:47 -0600

    update

--- bart-zero-shot/main.py
+++ bart-zero-shot/main.py
@@ -2,9 +2,14 @@ from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-def compute(text, candidate_labels, multi_class=False):
-    candidate_labels = ['travel', 'cooking', 'dancing']
-    result = classifier(text, candidate_labels, multi_class)
+def compute(text, candidate_labels, multi_label=False):
+    if multi_label:
+        result = classifier(text, candidate_labels, multi_label=True)
+    else:
+        result = classifier(text, candidate_labels)
 
     return result
 
+if __name__ == "__main__":
+    res = compute(text="let's go to the moon", candidate_labels=['astronomy', 'travel'], multi_label=False)
+    print(res)
