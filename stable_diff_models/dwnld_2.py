f950920ce76eb84728da4d7862438c2ca2fb4b56
John <johnbt@runbox.com>
2022-10-04 16:20:30 -0600

    update

---
+++ stable_diff_models/dwnld_2.py
@@ -0,0 +1,174 @@
+import os
+import streamlit as st
+from pathlib import Path
+
+dest = "/pebble_tmp/models/stable_diffusion_models"
+
+def exec_cmd(prompt):
+    res = os.popen(prompt)
+    output = res.readlines()
+    print(output)
+
+    return output
+
+def print_progress(msg, strmlit_ui):
+    st.write(msg) if strmlit_ui else print(msg)
+
+def dwnld_model(exceptions_log, address, target_location, source_model_name=None, target_model_name=None, strmlit_ui=False):
+    target_name = Path(target_model_name)
+
+    if not target_name.exists():
+        msg = f"Downloading {address} to {target_location}"
+        print_progress(msg, strmlit_ui)
+        try:
+            os.system(f'wget {address} -P {target_location}')
+            if target_model_name is not None:
+                os.system(f'mv {source_model_name} {target_model_name}')
+            print_progress("Done", strmlit_ui)
+        except Exception as e:
+            st.write(e) if strmlit_ui else exceptions_log.append([msg,e])
+    else:
+        msg = f"Skipping {target_name}"
+        print_progress(msg, strmlit_ui)
+    
+    return exceptions_log
+
+strmlit_ui = True
+class Models():
+    exceptions_log = []
+    dest = dest.strip("/").replace(" ", "_")
+
+    dest = os.path.expanduser("~/" + dest)
+
+    def modelSD():
+
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media', 
+                                    target_location = f'{dest}/models/ldm/stable-diffusion-v1/', 
+                                    source_model_name= f'{dest}/models/ldm/stable-diffusion-v1/sd-v1-4.ckpt?alt=media', 
+                                    target_model_name= f'{dest}/models/ldm/stable-diffusion-v1/model.ckpt', 
+                                    strmlit_ui=True)
+
+    def realESRGAN():
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth', 
+                                    target_location = f'{dest}/models/src/realesrgan/experiments/pretrained_models', 
+                                    source_model_name= f'{dest}/models/src/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus.pth', 
+                                    target_model_name= f'{dest}/models/src/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus.pth', 
+                                    strmlit_ui=True)
+        
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth', 
+                                    target_location = f'{dest}/models/src/realesrgan/experiments/pretrained_models', 
+                                    source_model_name= f'{dest}/models/src/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus_anime_6B.pth', 
+                                    target_model_name= f'{dest}/models/src/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus_anime_6B.pth', 
+                                    strmlit_ui=True)
+    
+    def GFPGAN():
+
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth', 
+                                    target_location = f'{dest}/models/src/gfpgan/experiments/pretrained_models', 
+                                    source_model_name= f'{dest}/models/src/gfpgan/experiments/pretrained_models/GFPGANv1.3.pth', 
+                                    target_model_name= f'{dest}/models/src/gfpgan/experiments/pretrained_models/GFPGANv1.3.pth', 
+                                    strmlit_ui=True)
+    
+        
+                                    
+    def modelLD():
+        if not Path(f'{dest}/models/src/latent-diffusion').exists():
+            os.system(f'cd {dest}/models/ ; git clone https://github.com/devilismyfriend/latent-diffusion.git')
+            os.system(f'mv {dest}/models/latent-diffusion  {dest}/models/src/latent-diffusion')
+
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://heibox.uni-heidelberg.de/f/31a76b13ea27482981b4/?dl=1', 
+                                    target_location = f'{dest}/models/src/latent-diffusion/experiments/pretrained_models', 
+                                    source_model_name= f'{dest}/models/src/latent-diffusion/experiments/pretrained_models/index.html?dl=1', 
+                                    target_model_name= f'{dest}/models/src/latent-diffusion/experiments/pretrained_models/project.yaml', 
+                                    strmlit_ui=True)
+
+        exceptions_log = dwnld_model(exceptions_log, 
+                                    address = 'https://heibox.uni-heidelberg.de/f/578df07c8fc04ffbadf3/?dl=1', 
+                                    target_location = f'{dest}/models/src/latent-diffusion/experiments/pretrained_models', 
+                                    source_model_name= f'{dest}/models/src/latent-diffusion/experiments/pretrained_models/index.html?dl=1', 
+                                    target_model_name= f'{dest}/models/src/latent-diffusion/experiments/pretrained_models/model.ckpt', 
+                                    strmlit_ui=True)
+    
+
+    # Stable Diffusion Conecpt Library
+    def SD_conLib():
+
+        if not Path(f'{dest}/models/custom/sd-concepts-library').exists():
+            os.system(
+                f'cd {dest}/models/ ; git clone https://github.com/sd-webui/sd-concepts-library models/custom/')
+            return st.write("Stable Diffusion Concept Library successfully installed !")
+
+    # Blip Model
+    def modelBlip():
+
+        if not Path(f'{dest}/models/custom/blip/model__base_caption.pth').exists():
+            # return st.write(f"Blip Model is to be installed !")
+            os.mkdir(f'{dest}/models/custom/blip')
+            os.system(f"wget -O {dest}/models/custom/blip/model__base_caption.pth https://cdn-lfs.huggingface.co/repos/cd/15/cd1551e1e53c5049819b5349e3e386c497a767dfeebb8e146ae2adb8f39c8d10/96ac8749bd0a568c274ebe302b3a3748ab9be614c737f3d8c529697139174086?response-content-disposition=attachment%3B%20filename%3D%22model__base_caption.pth%22")
+            return st.write(f"Blip model successfully installed")
+
+    # Waifu Diffusion v1.2
+    def modelWD():
+
+        if not Path(f'{dest}/models/custom/waifu-diffusion').exists():
+            os.system(
+                f"cd {dest}/models/ ;git clone https://huggingface.co/hakurei/waifu-diffusion models/custom/waifu-diffusion")
+            return st.write(f"Waifu Diffusion model successfully installed")
+
+    # Waifu Diffusion v1.2 Pruned
+    def modelWDP():
+
+        if not Path(f'{dest}/models/custom/pruned-waifu-diffusion').exists():
+            os.system(
+                f"cd {dest}/models/ ;git clone https://huggingface.co/crumb/pruned-waifu-diffusion models/custom/pruned-waifu-diffusion")
+            return st.write(f"Waifu Pruned model successfully installed")
+
+    # TrinArt Stable Diffusion v2
+    def modelTSD():
+
+        if not Path(f'{dest}/models/custom/trinart_stable_diffusion_v2').exists():
+            os.system(
+                f"cd {dest}/models/ ;git clone https://huggingface.co/naclbit/trinart_stable_diffusion_v2 models/custom/trinart_stable_diffusion_v2")
+            return st.write(f"TrinArt successfully installed !")
+
+    
+
+
+
+def st_ui():
+    st.title("Stable Diffusion models download")
+    if st.button("Stable Diffusion"):
+        Models.modelSD()
+
+    if st.button("RealESRGAN"):
+        Models.realESRGAN()
+
+    if st.button("GFPGAN"):
+        Models.GFPGAN()
+
+    if st.button("Latent Diffusion"):
+        Models.modelLD()
+
+    if st.button("SD Concept Lib"):
+        Models.SD_conLib()
+
+    if st.button("Blip"):
+        Models.modelBlip()
+
+    if st.button("Waifu Diffusion"):
+        Models.modelWD()
+
+    if st.button("Waifu Pruned"):
+        Models.modelLD()
+
+    if st.button("TrinArt SD"):
+        Models.modelTSD()
+
+if __name__ == "__main__":
+    st_ui()
+    
